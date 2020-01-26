406aa53ecc401384b31623fcc108b69fd0909507
JunyiZHONG <zhongjunyi.dev@gmail.com>
2020-01-26 13:16:14 +0100

    upload

---
+++ app/src/main/java/com/example/news/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.news;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.example.news.models.User;
+import com.example.news.ui.collection.CollectionFragment;
+import com.example.news.ui.explore.ExploreFragment;
+import com.example.news.ui.headlines.HeadlinesFragment;
+import com.example.news.ui.profile.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new HeadlinesFragment()).commit();
+        }
+    }
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.navigation_headlines:
+                            selectedFragment = new HeadlinesFragment();
+                            break;
+                        case R.id.navigation_collection:
+                            selectedFragment = new CollectionFragment();
+                            break;
+                        case R.id.navigation_explore:
+                            selectedFragment = new ExploreFragment();
+                            break;
+                        case R.id.navigation_profile:
+                            selectedFragment = new ProfileFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
+
+}
